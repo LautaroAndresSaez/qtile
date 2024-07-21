94cca9c5c1a1e90a11357d6e53b59a4c423dd08a
Lautaro Andres Saez <LautaroAndresSaez@gmail.com>
2024-07-21 12:13:02 -0300

    feat: cambios en el layout

--- config.py
+++ config.py
@@ -1,58 +1,30 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
+from themes.Dracula import Dracula
+from macros import *
 mod = "mod4"
 terminal = guess_terminal()
 
+colors = Dracula()
+
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], LEFT, lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], RIGHT, lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], DOWN, lazy.layout.down(), desc="Move focus down"),
+    Key([mod], UP, lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+
+    Key([mod, "shift"], LEFT, lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], RIGHT, lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], DOWN, lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], UP, lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "control"], LEFT, lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], RIGHT, lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], DOWN, lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], UP, lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -94,13 +66,13 @@ for vt in range(1, 8):
     )
 
 
-groups = [Group(i) for i in "123456789"]
-
+GROUPS_ICONS = [MAIN_ICON, CODE_ICON, MUSIC_ICON]
+groups = [Group(str(i)) for i in range(1, len(GROUPS_ICONS) + 1)]
 for i in groups:
     keys.extend(
         [
             # mod + group number = switch to group
-            Key(
+            Key(        
                 [mod],
                 i.name,
                 lazy.group[i.name].toscreen(),
@@ -121,7 +93,7 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(border_focus_stack=[colors["cyan"], colors["pink"]], border_width=4),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
@@ -137,15 +109,16 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans",
+    font=font,
     fontsize=12,
     padding=3,
+    background=colors["background"]
 )
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
@@ -153,17 +126,13 @@ screens = [
                 widget.WindowName(),
                 widget.Chord(
                     chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
+                        "launch": (colors["pink"], colors["cyan"]),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.TextBox(background=colors["background"], fmt="\ue0b2", foreground=colors["purple"], fontsize=30, padding=0),
+                widget.Clock(format="%a %I:%M %p %d/%m/%Y", background=colors["purple"]),
             ],
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
