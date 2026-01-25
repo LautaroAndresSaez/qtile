8b83269b454c356741c229fb803e19a8b6ba2286
Lautaro Andres Saez <lautaroandressaez@gmail.com>
2026-01-25 08:46:16 -0300

    fix: init migration x11 to wayland

---
+++ x11-config.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+
+from libqtile import bar, layout, qtile, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile import hook, qtile
+from settings.qtileManager import QTileManager
+
+mod = "mod4"
+terminal = "alacritty"
+
+qtileManager = QTileManager(
+    f'{os.environ["HOME"]}/.config/qtile/settings.default.json')
+
+settings = qtileManager.settings
+
+keys = qtileManager.keys
+layouts = qtileManager.layouts
+groups = qtileManager.groups
+screens = qtileManager.screens
+
+# Add key bindings to switch VTs in Wayland.
+# We can't check qtile.core.name in default config as it is loaded before qtile is started
+# We therefore defer the check until the key binding is run by using .when(func=...)
+for vt in range(1, 8):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(
+                func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
+
+
+widget_defaults = dict(
+    font=settings.font,
+    fontsize=15,
+    padding=3,
+    background=settings.colors.background
+)
+extension_defaults = widget_defaults.copy()
+
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# xcursor theme (string or None) and size (integer) for Wayland backend
+wl_xcursor_theme = None
+wl_xcursor_size = 24
+
+wmname = "LG3D"
+
+
+@hook.subscribe.startup_once
+def autostart():
+    # subprocess.Popen(["dunst"])
+    subprocess.Popen(["swaybg", '-i', '/home/lsaez/Pictures/gauss.jpg'])
+    subprocess.Popen(["kanshi"])
+
+
+@hook.subscribe.screen_change
+def restart_on_randr(_):
+    os.system("~/.config/qtile/monitor_change.sh")
+    qtile.cmd_reload_config()
